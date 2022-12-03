e00c9c39ec443e622fd9f67a66c7755b8897ab21
Ilya Kolesnikov <zloy-gamer@yandex.ru>
2022-12-03 20:47:03 +0500

    Add download images and put in filename to txt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import ast
 import os
 import datetime
 
+import requests
 import vk
 import logging.config
 from dotenv import load_dotenv
@@ -13,80 +14,103 @@ from date_format import get_date
 load_dotenv()
 
 API_VERSION = '5.131'  # версия VK API
-REQUEST_DELAY = 0.4  # Задержка между запросами к API
+REQUEST_DELAY = 0.4  # задержка между запросами к API
+PATH_TO_BACKUP = 'backup'
+FILE_MESSAGE_NAME = 'Чат WhatsApp с '
 TOKEN = os.getenv('TOKEN')  # токен вашего приложения VK для доступа к VK API
 # словарь с id членов диалога и имени контакта в телефонной книге
 # например, {0000000: 'Александр Пушкин'}
 MEMBER_NAMES = ast.literal_eval(os.getenv('MEMBER_NAMES'))
-PEER_ID = os.getenv('PEER_ID')  # ID чата в VK
-GROUP = os.getenv('GROUP')  # ID принадлежит групповому чату?
-
-api = vk.API(access_token=TOKEN, v=API_VERSION)
+PEER_ID = int(os.getenv('PEER_ID'))  # ID чата VK (+2000000000, если групповой)
+BASED_DIR = 'backup'
 
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
+api = vk.API(access_token=TOKEN, v=API_VERSION)
 
 
 def check_environment_variables() -> bool:
     """Функция проверяет доступность необходимых переменных окружения."""
-    return all([TOKEN, MEMBER_NAMES, PEER_ID, GROUP])
+    return all([TOKEN, MEMBER_NAMES, PEER_ID])
 
 
-def get_history_items(peer_id, offset=0, count=200, group=0):
-    if group:
-        peer_id += 2000000000
-    items = []
+def get_chat_title(peer_id: int) -> str:
+    try:
+        response = api.messages.getConversationsById(peer_ids=peer_id)
+    except Exception as error:
+        logger.error(f'Чат не найден: {error}')
+        sys.exit()
+
+    return response['items'][0]['chat_settings']['title']
+
+def get_history_items(peer_id: int,
+                      offset: int = 0,
+                      count: int = 200,
+                      ) -> list:
+    history_items = []
 
     logger.info(f'Начинаем загрузку сообщений')
     while True:
         try:
-            request = api.messages.getHistory(
-                offset=offset,
-                count=count,
-                peer_id=peer_id,
-                rev=1,
-            )
+            response = api.messages.getHistory(offset=offset,
+                                               count=count,
+                                               peer_id=peer_id,
+                                               rev=1,
+                                               )
         except Exception as error:
             logger.error(f'Сбой при загрузке: {error}')
             sys.exit()
-        if not request['items']:
-            logger.info(f'Больше сообщений нет')
+
+        if not response['items']:
+            logger.info(f'Все доступные сообщения загружены')
             break
 
-        offset += 200
-        items.extend(request['items'])
-        logger.info(f'Загружено {len(items)} сообщений')
+        history_items.extend(response['items'])
+        logger.info(f'Загружено {len(history_items)} сообщений')
         time.sleep(REQUEST_DELAY)
+        offset += 200
 
-    return items
+    return history_items
 
 
-def get_messages(items: list) -> list:
+def parse_messages(items: list, backup_path: str) -> list:
     messages = []
 
-    logger.info(f'Провожу парсинг сообщений')
+    logger.info(f'Провожу парсинг сообщений и скачиваю файлы...')
+    logger.info(f'Это может занять много времени и места на диске')
     for item in items:
         date = get_date(item['date'])
-        member = MEMBER_NAMES.get(item['from_id'])
+        member = MEMBER_NAMES.get(item['from_id'], f"id_{item['from_id']}")
         text = item['text']
-        urls = []
+        files = []
 
         if text:
             messages.append(f'{date} - {member}: {text}')
 
         for attachment in item['attachments']:
             if photo := attachment.get('photo'):
-                url = next((photo['sizes'][i]['url'] for i in
-                            range(len(photo['sizes'])) if
-                            photo['sizes'][i]['type'] == 'w'),
-                           photo['sizes'][-1]['url'])
-                urls.append(url)
-
-        if urls:
-            for url in urls:
-                messages.append(f'{date} - {member}: {url} (файл добавлен)')
+                files.append(get_image_from_message(photo, backup_path))
+        for file in files:
+            messages.append(f'{date} - {member}: {file} (файл добавлен)')
 
     return messages
+
+
+def get_image_from_message(photo: dict, backup_path: str) -> str:
+    file_name = str(photo['id']) + '.jpg'
+    path_to_file = backup_path + file_name
+    sizes = sorted(photo['sizes'], key=lambda d: d['type'])
+    url = next((sizes[i]['url'] for i in range(len(sizes)) if
+                sizes[i]['type'] == 'w'), sizes[-1]['url'])
+
+    try:
+        if not os.path.exists(path_to_file):
+            with open(path_to_file, 'wb') as f:
+                f.write(requests.get(url).content)
+    except Exception as error:
+        logger.error(f'Ошибка при скачивании картинки: {error}')
+
+    return file_name
 
 
 def main():
@@ -94,23 +118,26 @@ def main():
     start_time = datetime.datetime.now()
 
     if not check_environment_variables():
-        logger.critical(
-            'Отсутствуют необходимые переменные окружения'
-            'Работа программы завершена'
-        )
+        logger.critical('Отсутствуют необходимые переменные окружения'
+                        'Работа программы завершена')
         sys.exit()
+    chat_title = get_chat_title(peer_id=PEER_ID)
+    history_items = get_history_items(peer_id=PEER_ID,
+                                      offset=0)
+    backup_path = BASED_DIR + f'/{PEER_ID}/'
 
-    items = get_history_items(peer_id=int(PEER_ID), group=int(GROUP), offset=0)
+    if history_items:
+        if not os.path.exists(backup_path) and not os.path.isdir(backup_path):
+            os.makedirs(backup_path)
 
-    if items:
-        messages = get_messages(items)
-        file_name = 'backup_chat.txt'
-        logger.info(f'Сохраняю сообщения в файл {file_name}')
-        with open(file_name, 'w') as f:
-            for message in messages:
-                f.write(f'{message}\n')
+        messages = parse_messages(history_items, backup_path)
+        message_file = backup_path + f'Чат WhatsApp с {chat_title}.txt'
+        logger.info(f'Сохраняю сообщения в файл {message_file}')
+        with open(message_file, 'w') as f:
+            for message_file in messages:
+                f.write(f'{message_file}\n')
     else:
-        logger.info(f'В этом чате нет сообщений')
+        logger.warning(f'В этом чате нет сообщений')
 
     end_time = datetime.datetime.now()
     time_delta = end_time - start_time
