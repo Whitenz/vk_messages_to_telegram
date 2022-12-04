28327eda12e7732d609e8a0ef0aa654d4881dd85
Ilya Kolesnikov <zloy-gamer@yandex.ru>
2022-12-04 20:51:14 +0500

    Add format_text for replace ref to user

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import time
 import ast
@@ -16,14 +17,15 @@ load_dotenv()
 
 API_VERSION = '5.131'  # версия VK API
 REQUEST_DELAY = 0.4  # задержка между запросами к API
-PATH_TO_BACKUP = 'backup'
 FILE_MESSAGE_NAME = 'Чат WhatsApp с '
 TOKEN = os.getenv('TOKEN')  # токен вашего приложения VK для доступа к VK API
 # словарь с id членов диалога и имени контакта в телефонной книге
 # например, {0000000: 'Александр Пушкин'}
 MEMBER_NAMES = ast.literal_eval(os.getenv('MEMBER_NAMES'))
 PEER_ID = int(os.getenv('PEER_ID'))  # ID чата VK (+2000000000, если групповой)
-BASED_DIR = 'backup'
+DOC_TYPES = {3: '.jpg', 4: '.gif'}
+BACKUP_DIR = f'backup/{PEER_ID}/'
+REGEXP = re.compile(r'\[id\d*\|@\w*]')
 
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
@@ -36,50 +38,48 @@ def check_environment_variables() -> bool:
 
 
 def get_chat_title(peer_id: int) -> str:
+    """Функция возвращает заголовок чата для создания файла бэкапа."""
     try:
         response = api.messages.getConversationsById(peer_ids=peer_id)
     except Exception as error:
         logger.error(f'Чат не найден: {error}')
         sys.exit()
-
     return response['items'][0]['chat_settings']['title']
 
 
-def get_history_items(peer_id: int,
-                      offset: int = 0,
-                      count: int = 200,
-                      ) -> list:
+def get_history_items(peer_id: int, offset: int = 0, count: int = 200) -> list:
+    """
+    Функция загружает историю чата.
+    offset - начальное сообщение для загрузки, count - кол-во сообщений.
+    """
     history_items = []
-
     logger.info(f'Начинаем загрузку сообщений')
     while True:
         try:
             response = api.messages.getHistory(offset=offset,
                                                count=count,
                                                peer_id=peer_id,
-                                               rev=1,
-                                               )
+                                               rev=1)
         except Exception as error:
-            logger.error(f'Сбой при загрузке: {error}')
+            logger.error(f'Сбой при загрузке сообщений: {error}')
             sys.exit()
 
         if not response['items']:
             logger.info(f'Все доступные сообщения загружены')
             break
-
         history_items.extend(response['items'])
         logger.info(f'Загружено {len(history_items)} сообщений')
         time.sleep(REQUEST_DELAY)
-        offset += 200
-
+        offset += count
     return history_items
 
 
-def parse_messages(items: list, backup_path: str) -> list:
+def parse_messages(items: list) -> list:
+    """Парсинг объектов истории чата и сохранения бэкапа."""
     messages = []
-
     logger.info(f'Провожу парсинг сообщений и скачиваю файлы...')
     logger.info(f'Это может занять много времени и места на диске')
+
     for item in items:
         date = get_date(item['date'])
         member = MEMBER_NAMES.get(item['from_id'], f"id_{item['from_id']}")
@@ -88,13 +88,13 @@ def parse_messages(items: list, backup_path: str) -> list:
         docs = []
 
         if text:
-            messages.append(f'{date} - {member}: {text}')
+            messages.append(f'{date} - {member}: {format_text(text)}')
 
         for attachment in item['attachments']:
             if photo := attachment.get('photo'):
-                images.append(get_image_from_message(photo, backup_path))
+                images.append(get_image_from_message(photo))
             if doc := attachment.get('doc'):
-                if doc_url := get_doc_url_from_message(doc, backup_path):
+                if doc_url := get_file_from_message(doc):
                     docs.append(doc_url)
 
         for image in images:
@@ -105,37 +105,45 @@ def parse_messages(items: list, backup_path: str) -> list:
     return messages
 
 
-def get_image_from_message(photo: dict, backup_path: str) -> str:
+def format_text(original_text: str) -> str:
+    def convert_name(match_obj):
+        member_id = re.search(r'\d+', match_obj.group()).group(0)
+        return MEMBER_NAMES.get(int(member_id), f'id_{member_id}')
+
+    if REGEXP.search(original_text):
+        return re.sub(REGEXP, convert_name, original_text)
+    return original_text
+
+
+def get_image_from_message(photo: dict) -> str:
+    """Функция вытаскивает из объекта сообщения изображение."""
     image_name = f"{photo['owner_id']}_{photo['id']}.jpg"
-    path_to_image = backup_path + image_name
     sizes = sorted(photo['sizes'], key=lambda d: d['type'])
     url = next((sizes[i]['url'] for i in range(len(sizes)) if
                 sizes[i]['type'] == 'w'), sizes[-1]['url'])
+    return download_file(url, image_name)
 
+
+def get_file_from_message(doc: dict) -> Optional[str]:
+    """
+    Функция вытаскивает из объекта сообщения файл.
+    Если тип файла не указан в словаре DOC_TYPES, то возвращает None.
+    """
+    if doc['type'] in DOC_TYPES:
+        doc_name = f"{doc['owner_id']}_{doc['id']}{DOC_TYPES[doc['type']]}"
+        return download_file(doc['url'], doc_name)
+
+
+def download_file(url: str, file_name: str) -> str:
+    """Функция скачивает файл по ссылке, возвращает название файла."""
+    path_to_file = BACKUP_DIR + file_name
     try:
-        if not os.path.exists(path_to_image):
-            with open(path_to_image, 'wb') as f:
+        if not os.path.exists(path_to_file):
+            with open(path_to_file, 'wb') as f:
                 f.write(requests.get(url).content)
     except Exception as error:
-        logger.error(f'Ошибка при скачивании картинки: {error}')
-
-    return image_name
-
-
-def get_doc_url_from_message(doc: dict, backup_path: str) -> Optional[str]:
-    if doc['type'] in (3, 4):  # тип картинок и gif
-        file_name = f"{doc['owner_id']}_{doc['id']}.jpg"
-        path_to_file = backup_path + file_name
-        url = doc['url']
-
-        try:
-            if not os.path.exists(path_to_file):
-                with open(path_to_file, 'wb') as f:
-                    f.write(requests.get(url).content)
-        except Exception as error:
-            logger.error(f'Ошибка при скачивании файла: {error}')
-
-        return file_name
+        logger.error(f'Ошибка при скачивании или записи файла: {error}')
+    return file_name
 
 
 def main():
@@ -146,17 +154,16 @@ def main():
         logger.critical('Отсутствуют необходимые переменные окружения'
                         'Работа программы завершена')
         sys.exit()
+
     chat_title = get_chat_title(peer_id=PEER_ID)
-    history_items = get_history_items(peer_id=PEER_ID,
-                                      offset=15300)
-    backup_path = BASED_DIR + f'/{PEER_ID}/'
+    history_items = get_history_items(peer_id=PEER_ID, offset=15300)
 
     if history_items:
-        if not os.path.exists(backup_path) and not os.path.isdir(backup_path):
-            os.makedirs(backup_path)
+        if not os.path.exists(BACKUP_DIR) and not os.path.isdir(BACKUP_DIR):
+            os.makedirs(BACKUP_DIR)
 
-        messages = parse_messages(history_items, backup_path)
-        message_file = backup_path + f'Чат WhatsApp с {chat_title}.txt'
+        messages = parse_messages(history_items)
+        message_file = BACKUP_DIR + f'Чат WhatsApp с {chat_title}.txt'
         logger.info(f'Сохраняю сообщения в файл {message_file}')
         with open(message_file, 'w') as f:
             for message_file in messages:
